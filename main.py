e88a6ab8ca2bbe33fa06ef1e02a186b423967e5e
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2022-11-29 16:12:55 +0700

    try catch exception

--- main.py
+++ main.py
@@ -77,9 +77,15 @@ def video_feed():
 	id = 1
 
 	stream = instances.get(str(id))
-	return Response(stream.generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
-
+	try:
+		return Response(stream.generate(),
+			mimetype = "multipart/x-mixed-replace; boundary=frame")
+	except:
+		response = app.response_class(
+        response=json.dumps("Not Ready Yet"),
+        status=200,
+        mimetype='application/json')
+		return response
 @app.route("/stop")
 def stop():
 	global instances
